9183ab36ba33e19ca3877089dcf1d75b33a55f3b
Michał Grezel <dracomithril@gmail.com>
2023-06-17 00:11:27 +0200

    add menu

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,39 +1,36 @@
 import React from "react";
 
 export const Events = () => {
-    return (
-      <section className="events" id="events">
-        <div className="title">
-          <h2>W czym możemy pomóc</h2>
-          <div className="line"></div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <img src="img/sprzedazedit.jpg" alt="zdjecie" />
-          </div>
-
-          <div className="col">
-            <img src="img/zakupedit.jpg" alt="zdjecie" />
-          </div>
-
-          <div className="col">
-            <img src="img/wynajemedit.jpg" alt="zdjecie" />
-          </div>
+  return (
+    <section className="events" id="events">
+      <div className="title">
+        <h2>W czym możemy pomóc</h2>
+        <div className="line"></div>
+      </div>
+      <div className="grid grid-cols-2 col-span-2 gap-3">
+        <div className="col">
+          <img src="img/sprzedazedit.jpg" alt="zdjecie" />
         </div>
 
-        <div className="raw">
-          <div className="col">
-            <img src="img/wycenaedit.jpg" alt="zdjecie" />
-          </div>
-
-          <div className="col">
-            <img src="img/finansowanieedit.jpg" alt="zdjecie" />
-          </div>
-
-          <div className="col">
-            <img src="img/zarzadanieedit.jpg" alt="zdjecie" />
-          </div>
+        <div className="col">
+          <img src="img/zakupedit.jpg" alt="zdjecie" />
         </div>
-      </section>
-    );
+
+        <div className="col">
+          <img src="img/wynajemedit.jpg" alt="zdjecie" />
+        </div>
+        <div className="col">
+          <img src="img/wycenaedit.jpg" alt="zdjecie" />
+        </div>
+
+        <div className="col">
+          <img src="img/finansowanieedit.jpg" alt="zdjecie" />
+        </div>
+
+        <div className="col">
+          <img src="img/zarzadanieedit.jpg" alt="zdjecie" />
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,30 @@
+"use client";
+import { useState } from "react";
+
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <header className="top-0 left-0 fixed pl-4 pr-4 pt-2 bg-transparent flex justify-between items-center z-30 w-screen">
-      <a href="index.html" className="relative text-2xl text-[#ededed] font-semibold">
-        HOME PLUS
-      </a>
-      <div className="w-3 h-3">
-        <i className="bx bx-menu" id="close-icon"></i>
+    <header className="top-0 left-0 fixed pl-4 pr-4 pt-2 bg-transparent flex flex-col z-30 w-screen">
+      <div className="flex items-center justify-between">
+        <a
+          href="index.html"
+          className="relative text-2xl text-[#ededed] font-semibold"
+        >
+          HOME PLUS
+        </a>
+        <button
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+          }}
+          className="w-3 h-3"
+        >
+          <i
+            className={`bx ${isMenuOpen ? "bx-x" : "bx-menu"}`}
+            id="close-icon"
+          ></i>
+        </button>
       </div>
-      <nav className="navbar hidden">
+      <nav className={`flex flex-col items-end ${isMenuOpen ? "" : " hidden"}`}>
         <a href="#kup">KUP</a>
         <a href="sprzedaj.html">SPRZEDAJ</a>
         <a href="#WYNAJMIJ">WYNAJMIJ</a>
