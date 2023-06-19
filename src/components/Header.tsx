7923a8efd8db2b6208618d4208de1ae327575522
Michał Grezel <dracomithril@gmail.com>
2023-06-19 09:57:28 +0200

    header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,36 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Header.module.scss";
 import classNames from "classnames";
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 66) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {    
+    changeBackground();
+    // adding the event when scroll change background
+    window.addEventListener("scroll", changeBackground);
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header
+      className={
+        classNames(styles.header, {
+          [styles.headerScroll]: navbar,
+        })}
+    >
       <div className="flex items-center justify-between">
-        <a
-          href="/"
-          className={styles.logo}
-        >
+        <a href="/" className={styles.logo}>
           HOME PLUS
         </a>
         <button
@@ -20,17 +39,17 @@ export const Header = () => {
           }}
           className={styles.menuIcon}
         >
-          <i
-            className={`bx ${isMenuOpen ? "bx-x" : "bx-menu"}`}
-            id="menu-icon"
-          ></i>
+          <i className={`bx ${isMenuOpen ? "bx-x" : "bx-menu"}`} id="menu-icon"></i>
         </button>
       </div>
-      <nav className={classNames(styles.navbar,{
-        [styles.navbarOpen]: isMenuOpen,
-      })} onClick={()=> {
-        setIsMenuOpen(false);
-      }} >
+      <nav
+        className={classNames(styles.navbar, {
+          [styles.navbarOpen]: isMenuOpen,
+        })}
+        onClick={() => {
+          setIsMenuOpen(false);
+        }}
+      >
         <a href="/kup">KUP</a>
         <a href="/sprzedaj">SPRZEDAJ</a>
         <a href="/wynajmij">WYNAJMIJ</a>
